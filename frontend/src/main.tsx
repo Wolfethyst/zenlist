0401b56fd3caf95a55ba47e06207e9f6ea55c85a
Wolfethyst <Caiden.Lemmons@pm.me>
2026-02-10 09:11:21 -0600

    Fix: wrangler v3+ deploy, ChakraProvider usage, add wrangler.toml

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
+
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({});
 
 createRoot(document.getElementById('root')!).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <StrictMode>
       <App />
     </StrictMode>
